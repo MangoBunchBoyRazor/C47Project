819dc7835d99b5817174b99f8d5bf40f25018c25
MohamedRazeen102006 <mohd.rizwan@gmail.com>
2020-08-09 18:17:08 +0530

    changed css

--- Classes/form.js
+++ Classes/form.js
@@ -6,25 +6,28 @@ class Form{
         this.formDiv.id('form');
         this.formDiv.position(300,100);
         this.formDiv.size(200,400);
-        this.formDiv.style('border: 2px solid black; border-radius: 5px;');
-        this.formDiv.style('background-color: cornsilk;');
+        this.formDiv.style("opacity: 0;");
 
         this.heading = createElement('h1','Killer Mode On');
         this.heading.position(280,10);
+        this.heading.style("opacity: 0; transition: all 0.2s ease-out;")
+        this.heading.id('head');
 
-        this.nameLabel = createElement('label',"Your blob's Name:");
-        this.nameLabel.parent('#form');
-        this.nameLabel.position(10,20);
+        this.nameLabel = createElement('label',"This is the tale of:");
+        //this.nameLabel.parent('#form');
+        this.nameLabel.id('name');
+        this.nameLabel.style("opacity: 0; transition: 0.7s ease-out;");
 
         this.nameInp = createInput();
         this.nameInp.attribute('placeholder','name');
-        this.nameInp.style('position: relative;');
-        this.nameInp.position(10,50);
-        this.nameInp.parent('#form');
+        this.nameInp.id('inputname');
+        this.nameInp.style("opacity: 0; transiition: 0.9s ease-out;");
+        //this.nameInp.parent('#form');
 
         this.submit = createButton('Play');
-        this.submit.position(50,100);
-        this.submit.parent('#form');
+        //this.submit.parent('#form');
+        this.submit.id('submit');
+        this.submit.style('opacity: 0; transitiion: 1.1s ease-out;');
         this.submit.mousePressed(()=>{
             this.submitData();
         })
@@ -37,6 +40,14 @@ class Form{
                 playerCount: 0
             });
         })
+
+        setTimeout(() => {
+            this.heading.style("opacity: 1");
+            this.formDiv.style("opacity: 1");
+            this.nameInp.style("opacity: 1");
+            this.nameLabel.style("opacity: 1");
+            this.submit.style("opacity: 1");
+        }, 500);
     }
     submitData(){
         if(this.nameInp.value() == ""){
